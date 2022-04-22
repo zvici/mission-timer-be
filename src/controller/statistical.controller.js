bd1968bb2e4ac376d3fc8dc26a06b828d43384ee
zvici <nhatranthanh113@gmail.com>
2022-04-22 08:32:41 +0700

    feat: statistical a user

--- src/controller/statistical.controller.js
+++ src/controller/statistical.controller.js
@@ -42,4 +42,47 @@ const activityUsersStatistics = asyncHandler(async (req, res) => {
   }
 })
 
-export { activityUsersStatistics }
+const activityAUserStatistics = asyncHandler(async (req, res) => {
+  try {
+    const result = await Participants.find().populate({
+      path: 'task',
+      select: 'officeHours activity',
+      populate: {
+        path: 'activity',
+        select: 'year',
+        populate: {
+          path: 'year',
+          select: 'name',
+        },
+      },
+    })
+    const listUser = await User.findById(req.params.id)
+    if (!listUser) {
+      return errorRespone(
+        res,
+        404,
+        0,
+        'error',
+        'Không tìm thấy người dùng này!'
+      )
+    }
+    let resultC = {
+      id: listUser._id,
+      name: listUser.name,
+      userId: listUser.userId,
+      ...sumQuota(listUser.id, result),
+    }
+    return res.send({
+      code: 1,
+      msg: 'success',
+      message: 'Thông kê trạng thái hoạt động của giảng viên.',
+      data: {
+        statistic: resultC,
+      },
+    })
+  } catch (error) {
+    return errorRespone(res, 400, 0, 'error', error)
+  }
+})
+
+export { activityUsersStatistics, activityAUserStatistics }
--- src/routes/statisticalRoutes.js
+++ src/routes/statisticalRoutes.js
@@ -1,9 +1,13 @@
-import { activityUsersStatistics } from '../controller/statistical.controller.js'
+import {
+  activityUsersStatistics,
+  activityAUserStatistics,
+} from '../controller/statistical.controller.js'
 import { protect, admin } from '../middlewares/authMiddleware.js'
 import express from 'express'
 
 const router = express.Router()
 
 router.route('/').get(activityUsersStatistics)
+router.route('/user/:id').get(activityAUserStatistics)
 
 export default router
